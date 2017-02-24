a16024ef28d9b6f7ab106434658cd2fee2570058
Amogh Mannekote <msamogh@gmail.com>
2017-02-24 18:07:22 +0530

    One last commit

--- action.py
+++ action.py
@@ -1,20 +1,38 @@
 class Action(object):
 
-	def __init__(self, fn, driver, **kwargs):
+	def __init__(self, driver, fn, xpath=None, data=None, **kwargs):
 		self.fn = fn
 		self.driver = driver
+		self.xpath = xpath
+		self.data = data
 		self.kwargs = kwargs
 
 	def __call__(self):
-		self.fn(self.driver, **self.kwargs)
+		args = [arg for arg in [self.xpath, self.data] if arg]
+		if self.xpath:
+			element = self.driver.find_element_by_xpath(self.xpath)
+			if self.data:
+				self.fn(self.driver, element, self.data)
+			else:
+				self.fn(self.driver, element)
+		else:
+			self.fn(self.driver, *args, **self.kwargs)
 
-	@staticmethod
-	def navigate(driver, **kwargs):
-		driver.get(kwargs['url'])
 
+def navigate(driver, **kwargs):
+	driver.get(kwargs['url'])
 
+def fill_text_field(driver, input_field, data):
+	input_field.send_keys(data)
 
-class XPathAction(Action):
+def click(driver, element):
+	element.click()
 
-	def __init__(self, fn, driver, xpath):
-		self.
+from selenium.webdriver.support.ui import Select
+def select_option(driver, element, option_value):
+	select = Select(element)
+	select.select_by_value(option_value)
+
+import time
+def wait(driver, seconds):
+	time.sleep(seconds)
